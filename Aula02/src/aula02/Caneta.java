5921ff06a1007f68c284c6d76cf99f8128848b70
Maria Eduarda <107825119+mariaeduardavr@users.noreply.github.com>
2022-11-22 02:04:14 -0300

    Aula02

---
+++ Aula02/src/aula02/Caneta.java
@@ -0,0 +1,37 @@
+package aula02;
+class Caneta {
+
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+    
+    void status() {
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Cor " + this.cor);
+        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Carga: " + this.carga);
+        System.out.println("Está tampada? " + this.tampada);
+    }
+    
+    void rabiscar() {
+        if (this.tampada == true) {
+            System.out.println("Não posso rabiscar, pois a caneta está tampada!");
+        }
+        else {
+            System.out.println("Estou rabiscando! A caneta está destampada.");
+        }
+    }
+    
+    void tampar() {
+        this.tampada = true;
+    }
+    
+    void destampar() {
+        this.tampada = false;
+    }
+    
+    
+    
+}
